0f47b1d300cb167e8f172e30b7a6c8179e9f78e4
Randy Ainsworth <randyainsworth.789@gmail.com>
2025-05-18 16:54:11 -0700

    finished url routes

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from apartments.views import ApartmentView, ApartmentDetailView, CommentViewSet
+from apartments.views import ApartmentView, ApartmentDetailView, CommentViewSet, NoteViewSet, AddressViewSet, \
+    PointsOfInterestsViewSet, ActivitiesViewSet
 
 router = DefaultRouter()
 router.register('comments', CommentViewSet)
+router.register('notes', NoteViewSet)
+router.register('address', AddressViewSet)
+router.register('point-of-interests', PointsOfInterestsViewSet)
+router.register('activities', ActivitiesViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('apartments/', ApartmentView.as_view()),
