2e340fbfff977d29ec643442d10280cb1474dc8c
Randy Ainsworth <randyainsworth.789@gmail.com>
2025-05-18 16:15:48 -0700

    finished viewsets for remaining models and permission for only admin or readonly

--- apartments/views.py
+++ apartments/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.generics import  RetrieveUpdateDestroyAPIView, \
      ListCreateAPIView
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
 
 from .models import Apartment, Note, Address, Activities, PointsOfInterests, Comments
 from .permissions import IsAdminOrReadOnly, IsAuthorOrReadOnly
-from .serializers import  DetailedApartmentSerializer, CommentsSerializer
+from .serializers import DetailedApartmentSerializer, CommentsSerializer, NoteSerializer, PointsOfInterestsSerializer, \
+    AddressSerializer, ActivitiesSerializer
 
 
 # Create your views here.
@@ -31,4 +33,24 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(name=self.request.user)
+
+class NoteViewSet(ModelViewSet):
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
+    permission_classes = [IsAdminOrReadOnly]
+
+class PointsOfInterestsViewSet(viewsets.ModelViewSet):
+    queryset = PointsOfInterests.objects.all()
+    serializer_class = PointsOfInterestsSerializer
+    permission_classes = [IsAdminOrReadOnly]
+
+class AddressViewSet(viewsets.ModelViewSet):
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
+    permission_classes = [IsAdminOrReadOnly]
+
+class ActivitiesViewSet(viewsets.ModelViewSet):
+    queryset = Activities.objects.all()
+    serializer_class = ActivitiesSerializer
+    permission_classes = [IsAdminOrReadOnly]
 
