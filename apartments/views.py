c3f4787438dbdf70a5a1e45eb7885015491f419e
Randy Ainsworth <randyainsworth.789@gmail.com>
2025-05-19 18:08:09 -0700

    added some sorting, filtering and pagination

--- apartments/views.py
+++ apartments/views.py
@@ -5,10 +5,18 @@ from rest_framework.generics import  RetrieveUpdateDestroyAPIView, \
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
 
+#? sorting, filtering and pagination
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
+
+
 from .models import Apartment, Note, Address, Activities, PointsOfInterests, Comments
 from .permissions import IsAdminOrReadOnly, IsAuthorOrReadOnly
 from .serializers import DetailedApartmentSerializer, CommentsSerializer, NoteSerializer, PointsOfInterestsSerializer, \
     AddressSerializer, ActivitiesSerializer
+
+
 
 
 # Create your views here.
@@ -18,6 +26,14 @@ class ApartmentView(ListCreateAPIView):
     queryset = Apartment.objects.all()
     serializer_class = DetailedApartmentSerializer
     permission_classes = [IsAdminOrReadOnly]
+    filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
+    search_fields = ['name', 'description']
+    filterset_fields = ['number_of_beds','baths','pets_allowed','home_type']
+    ordering_fields = ['name', 'created_at']
+    pagination_class = PageNumberPagination
+
+
+
 
 
 class ApartmentDetailView(RetrieveUpdateDestroyAPIView):
@@ -34,10 +50,16 @@ class CommentViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(name=self.request.user)
 
+    filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
+    # filterset_fields = ['name']
+
 class NoteViewSet(ModelViewSet):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
     permission_classes = [IsAdminOrReadOnly]
+    filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
+    search_fields = ['description']
+
 
 class PointsOfInterestsViewSet(viewsets.ModelViewSet):
     queryset = PointsOfInterests.objects.all()
